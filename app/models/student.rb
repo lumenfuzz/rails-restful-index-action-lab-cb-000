8bd1bf298ee29ca3ec4fc308a2fa36eb486449f9
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-30 16:17:23 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,14 @@
 class Student < ActiveRecord::Base
+  @@students = []
+
+  def initialize
+    @@students << self
+  end
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def self.all
+
 end
